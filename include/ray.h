faae8e2ae5aefd0e7e18dd8e18c5c98b853833aa
Stephen Pasch <ssp2188@columbia.edu>
2026-07-20 15:48:41 -0700

    const at()

--- include/ray.h
+++ include/ray.h
@@ -13,7 +13,7 @@ class ray {
         const point3& origin() const {return orig;}
         const vec3& direction() const {return dir;}
 
-        point3 at( double t ){
+        const point3 at( double t ) const {
             return orig + t * dir;
         }
 
